768da20cf547bf50aafae7c8c471dee4293604a7
Latif Vedic <latif@vedicsoftcts.com>
2020-01-09 14:14:12 +0530

    Maven Demo 1st commit

---
+++ src/main/java/com/myvedic/mavendemo1/MavenDemo1.java
@@ -0,0 +1,14 @@
+package com.myvedic.mavendemo1;
+
+public class MavenDemo1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Maven Demo Project");
+        int i, n=10;
+        for(i=0;i< n;i++)
+        {
+            System.out.println(i);
+        }
+        }
+    }
